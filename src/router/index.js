e5151a0876805006a312934e5e7221981630a24b
lwj <3427348074@qq.com>
2022-11-06 17:02:42 +0800

    all

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/Home.vue'
+import City from '@/views/City.vue'
+import SpringView from '@/views/SpringView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/city/:city',
+    name:'City',
+    component: City,
+    props:true
+  },
+  {
+    path:'/spring/:citys',
+    name:'SpringView',
+    component: SpringView,
+    props:true
   }
 ]
 
